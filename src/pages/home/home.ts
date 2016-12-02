9c6b6425c5f01911ae525fd30a71b81cf2045e10
Patrick Reames <patrick.reames@gmail.com>
2016-12-01 22:58:26 -0600

    leaflet map on homepage!

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,30 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
+
+import * as Leaflet from 'leaflet';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit{
 
   constructor(public navCtrl: NavController) {
-    
+
   }
 
+  ngOnInit(): void {
+    this.drawMap();
+  }
+  drawMap(): void {
+    let map = Leaflet.map('map');
+    Leaflet.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoicGF0cmlja3IiLCJhIjoiY2l2aW9lcXlvMDFqdTJvbGI2eXUwc2VjYSJ9.trTzsdDXD2lMJpTfCVsVuA', {
+      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+      maxZoom: 18
+    }).addTo(map);
+
+    //web location test
+    map.locate({ setView: true, maxZoom: 8 });
+
+  }
 }
